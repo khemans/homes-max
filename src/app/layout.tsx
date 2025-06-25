5a9e5922a50b98d17643bedf6ad66a1fd678a7e5
khemans <khemans@remax.com>
2025-06-25 08:21:47 -0600

    Add search to not found property page.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
+import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
+        <Suspense>
+          <Header />
+        </Suspense>
         <main className="pt-0">
           {children}
         </main>
