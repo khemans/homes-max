3ed167af11310d4c3af9ec036d07609757f810ee
khemans <khemans@remax.com>
2025-06-25 08:18:00 -0600

    Add search to not found property page.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,19 @@
 "use client";
 import { Suspense } from "react";
+import { useSearchParams } from "next/navigation";
 
 function NotFoundContent() {
-  // You can use useSearchParams() here if needed
+  const searchParams = useSearchParams();
+  const paramsString = searchParams ? searchParams.toString() : "";
   return (
     <div style={{ textAlign: "center", marginTop: 40 }}>
       <h1>404 - Page Not Found</h1>
       <p>Sorry, we couldn&apos;t find that page.</p>
+      {paramsString && (
+        <div style={{ marginTop: 16, color: '#888', fontSize: 14 }}>
+          <strong>URL Parameters:</strong> {paramsString}
+        </div>
+      )}
     </div>
   );
 }
--- src/app/property/not-found.tsx
+++ src/app/property/not-found.tsx
@@ -1,11 +1,19 @@
 "use client";
 import { Suspense } from "react";
+import { useSearchParams } from "next/navigation";
 
 function PropertyNotFoundContent() {
+  const searchParams = useSearchParams();
+  const paramsString = searchParams ? searchParams.toString() : "";
   return (
     <div style={{ textAlign: "center", marginTop: 40 }}>
       <h1>Property Not Found</h1>
       <p>Sorry, we couldn&apos;t find that property.</p>
+      {paramsString && (
+        <div style={{ marginTop: 16, color: '#888', fontSize: 14 }}>
+          <strong>URL Parameters:</strong> {paramsString}
+        </div>
+      )}
     </div>
   );
 }
